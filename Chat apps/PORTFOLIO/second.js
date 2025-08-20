c2518e49fc76d8c019d468094272361b248fccd7
Vinay Asati <vinayasati90@gmail.com>
2025-08-20 12:00:25 +0530

    Delete Chat apps directory

--- Chat apps/PORTFOLIO/second.js
+++
@@ -1,8 +0,0 @@
-// Simple smooth scroll effect
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
--- Chat apps/YouTube like Plateform/social.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.querySelector(".menu-icon");
-const sidebar= document.querySelector(".sidebar");
-const Container = document.querySelector(".container");
-menuIcon.addEventListener("click" , ()=>{
-    sidebar.classList.toggle("small-sidebar");
-    Container.classList.toggle("large-container");
-})
-
